98ec9fd09aebb3a005412823cec63b119264bba1
abghifareihand <info.abghifareihan@gmail.com>
2023-01-16 19:06:43 +0700

    fix pilih seat, menggunan instance baru listfrom

--- lib/cubit/seat_cubit.dart
+++ lib/cubit/seat_cubit.dart
@@ -10,7 +10,7 @@ class SeatCubit extends Cubit<List<String>> {
       state.remove(id);
     }
     print(state);
-    emit(state);
+    emit(List.from(state));
   }
 
   bool isSelected(String id) {
